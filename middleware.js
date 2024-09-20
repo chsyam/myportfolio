4be11c7fcdc841ae3d0b52ba8b063dbce0721943
Syam Kumar Chintakayala <syamkumar6845@gmail.com>
2024-09-21 03:08:45 +0530

    cookies error resolved in middleware.js

--- middleware.js
+++ middleware.js
@@ -1,26 +1,37 @@
+import jwt from 'jsonwebtoken'
+import CryptoJS from 'crypto-js';
 import { NextResponse } from 'next/server';
 
 export async function middleware(request) {
     const req_cookies = request.cookies.get('token');
-    const token = req_cookies['value'];
-
-    if (!token) {
+    if (req_cookies == undefined) {
         return NextResponse.redirect(new URL('/login', request.url));
     }
+    const token = req_cookies['value'];
+    console.log("token =>", token);
 
+    if (!token) {
+        console.log("token not found");
+        return NextResponse.redirect(new URL('/login', request.url));
+    }
     try {
-        const res = await fetch('https://myportfolio-henna-six.vercel.app/api/auth/protected', {
-            headers: {
-                Authorization: token,
-            },
-        });
-        console.log(res.status);
+        const hash = CryptoJS.SHA256(process.env.JWT_SECRET).toString();
+        console.log("hash", hash);
+        // const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        // console.log("decoded", decoded)
+        // const res = await fetch('https://myportfolio-henna-six.vercel.app/api/auth/protected', {
+        //     headers: {
+        //         Authorization: token,
+        //     },
+        // });
 
-        if (!res.ok) {
+        // console.log(res)
+        if (!hash) {
             throw new Error('Token validation failed');
         }
-
-        return NextResponse.next();
+        if (hash) {
+            return NextResponse.next();
+        }
     } catch (error) {
         console.error(error);
         return NextResponse.redirect(new URL('/login', request.url));
--- pages/api/auth/protected.js
+++ pages/api/auth/protected.js
@@ -3,7 +3,9 @@ import jwt from 'jsonwebtoken'
 export default async function handler(req, res) {
     try {
         const authHeader = req.rawHeaders[5];
+        console.log("authHeader, process.env.JWT_SECRET", authHeader, process.env.JWT_SECRET)
         const decoded = jwt.verify(authHeader, process.env.JWT_SECRET)
+        console.log("decoded", decoded)
 
         if (!decoded) {
             return res.status(400).json({ message: 'Invalid token' })
